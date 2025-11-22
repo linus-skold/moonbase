a0333e500cd277be0a8a68e86b03ee78e1ed79bc
Linus <contact@linusskold.com>
2025-11-22 18:10:05 +0100

    feat: Implemented integration mapper

---
+++ components/integration/integrations.ts
@@ -0,0 +1,9 @@
+
+import AdoIntegration from "@/components/integration/ado/index";
+
+import type { Integration } from "./IntegrationProvider";
+
+
+export const integrations: Integration[] = [
+  AdoIntegration,
+];
