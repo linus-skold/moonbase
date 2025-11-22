bc8a58ec6e2db52855859f13bfbcf72674a53f18
Linus <contact@linusskold.com>
2025-11-22 18:59:47 +0100

    feat: added collapsible groups to the sidebar to see available integrations&instances

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,4 +1,5 @@
-import { Calendar, Home, Inbox, Search, Settings, Layers } from "lucide-react";
+"use client";
+import { Calendar, Home, Inbox, Search, Settings, Layers, ChevronRight } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
   SidebarFooter,
@@ -10,9 +11,19 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
   SidebarMenuBadge,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
 } from "@/components/ui/sidebar";
 import { CustomSidebar } from "@/components/sidebar/custom-sidebar";
-import { VscGithub } from "react-icons/vsc";
+import { VscGithub, VscAzureDevops } from "react-icons/vsc";
+import { loadConfig as loadAdoConfig } from "@/lib/ado/storage";
+import React from "react";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
 
 // Menu items.
 const items = [
@@ -34,6 +45,14 @@ interface AppSidebarProps {
 };
 
 export function AppSidebar(props?: AppSidebarProps) {
+  const [adoInstances, setAdoInstances] = React.useState<any[]>([]);
+  const [isAdoOpen, setIsAdoOpen] = React.useState(true);
+
+  React.useEffect(() => {
+    const config = loadAdoConfig();
+    setAdoInstances(config?.instances || []);
+  }, []);
+
   return (
     <CustomSidebar>
       <SidebarContent>
@@ -55,23 +74,43 @@ export function AppSidebar(props?: AppSidebarProps) {
           </SidebarGroupContent>
         </SidebarGroup>
 
-        {/* <SidebarGroup>
-          <SidebarGroupLabel>My Inboxes</SidebarGroupLabel>
+        <SidebarGroup>
+          <SidebarGroupLabel>Integrations</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
-              {integrations.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
+              {adoInstances.length > 0 && (
+                <Collapsible
+                  open={isAdoOpen}
+                  onOpenChange={setIsAdoOpen}
+                  className="group/collapsible"
+                >
+                  <SidebarMenuItem>
+                    <CollapsibleTrigger asChild>
+                      <SidebarMenuButton>
+                        <VscAzureDevops className="h-4 w-4" />
+                        <span>Azure DevOps</span>
+                        <ChevronRight className="ml-auto h-4 w-4 transition-transform group-data-[state=open]/collapsible:rotate-90" />
+                      </SidebarMenuButton>
+                    </CollapsibleTrigger>
+                    <CollapsibleContent>
+                      <SidebarMenuSub>
+                        {adoInstances.map((instance) => (
+                          <SidebarMenuSubItem key={instance.id}>
+                            <SidebarMenuSubButton asChild>
+                              <a href={`/inbox/ado-${instance.id}`}>
+                                <span>{instance.name}</span>
+                              </a>
+                            </SidebarMenuSubButton>
+                          </SidebarMenuSubItem>
+                        ))}
+                      </SidebarMenuSub>
+                    </CollapsibleContent>
+                  </SidebarMenuItem>
+                </Collapsible>
+              )}
             </SidebarMenu>
           </SidebarGroupContent>
-        </SidebarGroup> */}
+        </SidebarGroup>
       </SidebarContent>
       <SidebarFooter>
         <div className="w-full flex justify-end">
