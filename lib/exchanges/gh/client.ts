bb99800afbcf38c49125c67d84ad840a952549dc
Linus <contact@linusskold.com>
2025-12-04 01:39:51 +0100

    feat(api): simplified the github client

--- lib/exchanges/gh/client.ts
+++ lib/exchanges/gh/client.ts
@@ -1,97 +1,41 @@
 import { Octokit } from "@octokit/rest";
-import type { GhInstance } from "./schema/instance.schema";
-
-type OctokitRepository = Awaited<ReturnType<Octokit['rest']['repos']['listForAuthenticatedUser']>>['data'][0];
-type OctokitPullRequest = Awaited<ReturnType<Octokit['rest']['pulls']['list']>>['data'][0];
-type OctokitIssue = Awaited<ReturnType<Octokit['rest']['issues']['listForRepo']>>['data'][0];
-type OctokitSearchIssue = Awaited<ReturnType<Octokit['rest']['search']['issuesAndPullRequests']>>['data']['items'][0];
+import { GhIntegrationInstance } from "./schema/config.schema";
+import { IssueSearchResultItem, IssueSearchResultItemSchema } from "./schema/api";
 
 export class GhClient {
   private octokit: Octokit;
   private username: string;
 
-  constructor(instance: GhInstance) {
+  constructor(instance: GhIntegrationInstance) {
     this.octokit = new Octokit({
       auth: instance.personalAccessToken,
     });
     this.username = instance.username;
   }
 
-  async getAuthenticatedUser(): Promise<{ login: string; id: number }> {
-    const { data } = await this.octokit.rest.users.getAuthenticated();
-    return { login: data.login, id: data.id };
-  }
-
-  async getRepositories() {
-    const { data } = await this.octokit.rest.repos.listForAuthenticatedUser({
-      sort: "updated",
+  private async searchForItems(query: string): Promise<IssueSearchResultItem[]> {
+    const { data } = await this.octokit.rest.search.issuesAndPullRequests({
+      q: query,
       per_page: 100,
     });
-    return data;
+    return data.items.map(item => IssueSearchResultItemSchema.parse(item));
   }
 
-  async getPullRequestsAssignedToMe(): Promise<OctokitSearchIssue[]> {
+  async getPullRequestsAssignedToMe(): Promise<IssueSearchResultItem[]> {
     // Search for PRs where the current user is assigned as a reviewer or assignee
     const searchQuery = `is:pr is:open review-requested:${this.username}`;
-    const { data } = await this.octokit.rest.search.issuesAndPullRequests({
-      q: searchQuery,
-      per_page: 100,
-    });
-    return data.items;
+    return this.searchForItems(searchQuery);
   }
 
-  async getPullRequestsCreatedByMe(): Promise<OctokitSearchIssue[]> {
+  async getPullRequestsCreatedByMe(): Promise<IssueSearchResultItem[]> {
     const searchQuery = `is:pr is:open author:${this.username}`;
-    const { data } = await this.octokit.rest.search.issuesAndPullRequests({
-      q: searchQuery,
-      per_page: 100,
-    });
-    return data.items;
+    return this.searchForItems(searchQuery);
   }
 
-  async getIssuesAssignedToMe(): Promise<OctokitSearchIssue[]> {
+  async getIssuesAssignedToMe(): Promise<IssueSearchResultItem[]> {
     // Search for issues (not PRs) assigned to the current user
     const searchQuery = `is:issue is:open assignee:${this.username}`;
-    const { data } = await this.octokit.rest.search.issuesAndPullRequests({
-      q: searchQuery,
-      per_page: 100,
-    });
-    return data.items;
+    return this.searchForItems(searchQuery);
   }
 
-  async getRepositoryPullRequests(
-    owner: string,
-    repo: string,
-    state: "open" | "closed" | "all" = "open"
-  ) {
-    const { data } = await this.octokit.rest.pulls.list({
-      owner,
-      repo,
-      state,
-      per_page: 100,
-    });
-    return data;
-  }
-
-  async getRepositoryIssues(
-    owner: string,
-    repo: string,
-    state: "open" | "closed" | "all" = "open"
-  ) {
-    const { data } = await this.octokit.rest.issues.listForRepo({
-      owner,
-      repo,
-      state,
-      per_page: 100,
-    });
-    return data;
-  }
-
-  async getRepository(owner: string, repo: string) {
-    const { data } = await this.octokit.rest.repos.get({
-      owner,
-      repo,
-    });
-    return data;
-  }
 }
